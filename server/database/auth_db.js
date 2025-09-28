6f682804a11351fe00d00cf703a57af75c340c23
pmaass53 <pippinmaass53@gmail.com>
2025-09-28 19:37:16 -0300

    Update auth_db.js

--- server/database/auth_db.js
+++ server/database/auth_db.js
@@ -1,1 +1,25 @@
+console.log("auth_db.js: Initiating Authorization Database...")
 
+// import sqlite3 package for database
+import sqlite3 from "sqlite3"
+// set constant for path to auth database
+// NOTE: path is relative to server.js
+const AUTH_DB_PATH = "./database/data/auth.db"
+
+const AUTHDB = new sqlite3.Database(AUTH_DB_PATH, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err1) => {
+  if (err1) {
+    console.log("auth_db.js: Error Opening/Creating Auth Database")
+    console.error(err1)
+  } else {
+    AUTHDB.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP)", (err2) => {
+      if (err2) {
+        console.log("auth_db.js: Error Creating Tables Auth Database")
+        console.error(err2)
+      } else {
+        console.log("auth_db.js: Auth Database Setup Complete")
+      }
+    })
+  }
+})
+
+export default AUTHDB
