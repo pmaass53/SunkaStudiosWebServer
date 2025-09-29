29f1bcb5199fd2a3415b0c513e598cef65b3ab40
pmaass53 <pippinmaass53@gmail.com>
2025-09-29 18:11:07 -0300

    Update homepage.js

--- server/apps/homepage.js
+++ server/apps/homepage.js
@@ -1,11 +1,11 @@
 import express from "express"
 import fs from "fs"
 
-const homepage = express()
+const homepage_app = express()
 
-homepage.get("/", (req, res) => {
+homepage_app.get("/", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"})
   res.end(fs.readFileSync("./public/index.html"))
 })
 
-export default homepage
+export default homepage_app
