e82180c88423dda8120dd2d0c50e560ab5deee78
pmaass53 <pippinmaass53@gmail.com>
2025-09-29 18:10:44 -0300

    Update auth.js
    minor changes

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,13 +1,8 @@
-console.log("auth.js: Initializing Authentication Server...")
-
 import express from "express"
 import fs from "fs"
 
 const auth_app = express()
 
-// configure server: TODO: add JSON support
-
-// routes
 auth_app.get("/", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"})
   res.end(fs.readFileSync("./public/auth.html"))
