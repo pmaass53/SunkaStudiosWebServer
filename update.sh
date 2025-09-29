57ccc1ad6838f65259c46290f311da69e8690aab
pmaass53 <pippinmaass53@gmail.com>
2025-09-29 10:31:23 -0300

    Update update.sh
    finished update code for server

--- update.sh
+++ update.sh
@@ -4,8 +4,15 @@ set -e
 APP_DIR="/home/ubuntu/latest/SunkaStudiosWebServer"
 REPO="https://github.com/pmaass53/SunkaStudiosWebServer"
 
+echo "Stopping Server"
+pm2 stop webserver || true
+
+echo "Downloading Newest Version"
 git fetch origin main
 git reset --hard origin/main
 
-# fix deps
+echo "Installing Packages"
 npm install --omit-dev
+
+echo "Starting Server"
+pm2 start /home/ubuntu/SunkaStudiosWebServer/server/serve.js --name webserver
