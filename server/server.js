f27bfccfeb79a520f24fd7f7278df0a936d986e3
pmaass53 <pippinmaass53@gmail.com>
2025-09-28 22:27:19 -0300

    Update server.js
    minor tweaks (comments, debug)

--- server/server.js
+++ server/server.js
@@ -6,12 +6,17 @@ import vhost from "vhost"
 
 console.log("Importing Modules & Constants...")
 const PORT = 8080
+// import various subdomains
 import auth_app from "./apps/auth.js"
 
 console.log("Creating Server...")
 const app = express()
 
 console.log("Configuring Server...")
+// use vhost to redirect requests to appropriate servers
 app.use(vhost("auth.localhost", auth_app))
 
-app.listen(PORT)
+// listen for requests
+app.listen(PORT, ()=> {
+  console.log(`Listening On Port ${PORT}`)
+})
