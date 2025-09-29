d8901cb8b7161bfd70982d58ac04477eff994d03
pmaass53 <pippinmaass53@gmail.com>
2025-09-29 18:54:39 -0300

    Create paths.js
    fix relative path bug

---
+++ server/utils/paths.js
@@ -0,0 +1,11 @@
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export const PROJECT_ROOT = path.resolve(__dirname, "..")
+export const SERVER_DIR = __dirname
+export const PUBLIC_DIR = path.join(__dirname, "public")
+export const APPS_DIR = path.join(__dirname, "apps")
+export const DATABASE_DIR = path.join(__dirname, "database")
