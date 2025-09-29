688c763d6e5525376d2b09ff3ea6b218ef6a3c9f
pmaass53 <pippinmaass53@gmail.com>
2025-09-29 19:01:23 -0300

    Update homepage.js
    fix homepage.html to use paths from paths.js

--- server/apps/homepage.js
+++ server/apps/homepage.js
@@ -1,11 +1,14 @@
 import express from "express"
 import fs from "fs"
+import path from "path"
+
+import { PUBLIC_DIR } from "./paths.js"
 
 const homepage_app = express()
 
 homepage_app.get("/", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"})
-  res.end(fs.readFileSync("./public/index.html"))
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "index.html")))
 })
 
 export default homepage_app
