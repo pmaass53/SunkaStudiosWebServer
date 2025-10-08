0a540a79fd7f0d3992f1ba687a0390cfdc7e39bd
pmaass53 <pippinmaass53@gmail.com>
2025-10-08 19:22:31 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,12 +11,16 @@ import morgan from "morgan"
 import { fileURLToPath } from "url"
 
 console.log("Importing Modules & Constants...")
+// internal port
+// DO NOT MODIFY
 export const PORT = 8080
+// import relative paths
 import { APPS_DIR } from "./paths.js"
 import { SSL_DIR } from "./paths.js"
 // import various subdomains
 const auth_app = (await import(path.join(APPS_DIR, "auth.js"))).default
 const homepage_app = (await import(path.join(APPS_DIR, "homepage.js"))).default
+const controlpanel_app = (await import(path.join(APPS_DIR, "controlpanel.js"))).default
 
 console.log("Creating Server...")
 const app = express()
@@ -30,6 +34,8 @@ app.use(helmet())
 app.use(morgan(":method :url :status - :response-time ms"))
 // use vhost to redirect requests to appropriate servers
 app.use(vhost("auth.sunkastudios.xyz", auth_app))
+app.use(vhost("controlpanel.sunkastudios.xyz", controlpanel_app))
+// serve rest to homepage app
 app.use(homepage_app)
 
 // listen for requests
