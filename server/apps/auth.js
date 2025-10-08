6273213c83cec2b1a25ad6bdaa5070be55223d90
pmaass53 <pippinmaass53@gmail.com>
2025-10-08 20:18:16 -0300

    Update auth.js

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -12,6 +12,11 @@ auth_app.get("/", (req, res) => {
   res.redirect(301, `/${LATEST_VERSION}/index.html`)
 })
 
+auth_app.get("/v1/ain.html", (req, res) => {
+  res.writeHead(200, {"Content-Type": "text/html"})
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "ain.html")))
+})
+
 auth_app.get("/v1/index.html", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"})
   res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "index.html")))
