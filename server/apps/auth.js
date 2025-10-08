7dfbdf7ebc7090177425bb9df1fc29bd20a83b53
pmaass53 <pippinmaass53@gmail.com>
2025-10-08 19:42:13 -0300

    Update auth.js

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -6,9 +6,15 @@ import { PUBLIC_DIR } from "../paths.js"
 
 const auth_app = express()
 
+const LATEST_VERSION = "v1"
+
 auth_app.get("/", (req, res) => {
+  res.redirect(301, `/${LATEST_VERSION}/index.html`)
+})
+
+auth_app.get("/v1/index.html", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"})
-  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth.html")))
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "index.html")))
 })
 
 export default auth_app
