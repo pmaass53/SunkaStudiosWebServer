77957db0c51920fe7e7a48b35262017e90d03275
pmaass53 <pippinmaass53@gmail.com>
2025-10-27 18:38:24 -0300

    Add favicon.ico route to homepage app

--- server/apps/homepage.js
+++ server/apps/homepage.js
@@ -10,5 +10,9 @@ homepage_app.get("/", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"})
   res.end(fs.readFileSync(path.join(PUBLIC_DIR, "index.html")))
 })
+homepage_app.get("/favicon.ico", (req, res) => {
+  res.writeHead(200, {"Content-Type": "image/x-icon"})
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "favicon.ico")))
+})
 
 export default homepage_app
