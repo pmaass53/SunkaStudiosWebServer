0bdd897d1f9e6e1ba14fe95501fff8943eb11e56
pmaass53 <pippinmaass53@gmail.com>
2025-10-28 19:58:03 -0300

    Add signup routes in auth.js

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -39,6 +39,21 @@ auth_app.get("/v1/login/main.css", (req, res) => {
   res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "login", "main.css")))
 })
 
+auth_app.get("/v1/signup/index.html", (req, res) => {
+  res.writeHead(200, {"Content-Type": "text/html"})
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "signup", "index.html")))
+})
+
+auth_app.get("/v1/signup/main.js", (req, res) => {
+  res.writeHead(200, {"Content-Type": "text/javascript"})
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "signup", "main.js")))
+})
+
+auth_app.get("/v1/signup/main.css", (req, res) => {
+  res.writeHead(200, {"Content-Type": "text/css"})
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "signup", "main.css")))
+})
+
 // login handling
 auth_app.post("/v1/login/api/postdata", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"})
