33c94c804a955a88a62c2a3dfbcf2603eb3162ed
pmaass53 <pippinmaass53@gmail.com>
2025-10-28 19:46:00 -0300

    Refactor auth routes for login functionality

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -14,35 +14,37 @@ auth_app.use(express.text({ type: "text/plain" }))
 auth_app.use(express.urlencoded({ extended: true }))
 
 auth_app.get("/", (req, res) => {
-  res.redirect(301, `/${LATEST_VERSION}/index.html`)
+  res.redirect(301, `/${LATEST_VERSION}/login/index.html`)
 })
 
 auth_app.get("/v1/ain.html", (req, res) => {
-  res.writeHead(200, {"Content-Type": "text/html"})
-  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "ain.html")))
+  // res.writeHead(200, {"Content-Type": "text/html"})
+  // res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "login", "ain.html")))
+  res.writeHead(501, { "Content-Type": "text/plain" })
+  res.end("AIN login is not yet supported")
 })
 
-auth_app.get("/v1/index.html", (req, res) => {
+auth_app.get("/v1/login/index.html", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"})
-  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "index.html")))
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "login", "index.html")))
 })
 
-auth_app.get("/v1/main.js", (req, res) => {
+auth_app.get("/v1/login/main.js", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/javascript"})
-  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "main.js")))
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "login", "main.js")))
 })
 
-auth_app.get("/v1/main.css", (req, res) => {
+auth_app.get("/v1/login/main.css", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/css"})
-  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "main.css")))
+  res.end(fs.readFileSync(path.join(PUBLIC_DIR, "auth", "v1", "login", "main.css")))
 })
 
 // login handling
-auth_app.post("/v1/api/login", (req, res) => {
+auth_app.post("/v1/login/api/postdata", (req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"})
   const { username, password } = req.body
   console.log(`Login Request for ${username}: '${password}'`)
-  res.end("login?")
+  res.end("Login is not yet fully supported")
 })
 
 export default auth_app
